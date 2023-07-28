077d3876cf4eba827c3a7f15cad07c02908a714e
seminchoi <choicco89@gmail.com>
2023-07-28 22:07:04 +0900

    2장 : Junit Test ver.1 - addAndGet() 메서드 테스트

---
+++ src/UserDaoTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+
+    @Test
+    public void addAndGet() throws SQLException {
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao userDao = ac.getBean("userDao", UserDao.class);
+
+        userDao.deleteAll();
+
+        User addUser = new User();
+        addUser.setId("semin");
+        addUser.setUserName("최세민");
+        addUser.setPassword("1010");
+
+        userDao.add(addUser);
+
+        User getUser = userDao.getById("semin");
+
+        Assertions.assertEquals(addUser.getUserName(), getUser.getUserName());
+        Assertions.assertEquals(addUser.getPassword(), getUser.getPassword());
+    }
+}
