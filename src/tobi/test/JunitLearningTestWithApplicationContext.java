b064f907763955dae8cac2d3cde335d84d8f910c
seminchoi <choicco89@gmail.com>
2023-07-30 17:48:24 +0900

    2장: Learing Test ver.2 - ApplicationContext를 이용한 Junit 학습 테스트

---
+++ src/tobi/test/JunitLearningTestWithApplicationContext.java
@@ -0,0 +1,36 @@
+//package tobi.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/tobi/junitTestContext.xml")
+public class JunitLearningTestWithApplicationContext {
+    @Autowired
+    ApplicationContext applicationContext;
+
+    static ApplicationContext curContext;
+
+    @Test
+    public void test1() {
+        Assertions.assertTrue(curContext == null || curContext == applicationContext);
+        curContext = applicationContext;
+    }
+
+    @Test
+    public void test2() {
+        Assertions.assertTrue(curContext == null || curContext == applicationContext);
+        curContext = applicationContext;
+    }
+
+    @Test
+    public void test3() {
+        Assertions.assertTrue(curContext == null || curContext == applicationContext);
+        curContext = applicationContext;
+    }
+}
