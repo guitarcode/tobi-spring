231db7f5db5b4fb00db0c5dae6ebbad1c68f1cb2
seminchoi <choicco89@gmail.com>
2023-07-30 16:29:32 +0900

    2장: Learing Test ver.1 - Junit 모듈에 대한 학습 테스트 - 테스트 메서드 마다 새로운 테스트 클래스 오브젝트를 만드는 것을 검증

---
+++ src/tobi/test/JunitLearningTest.java
@@ -0,0 +1,29 @@
+package tobi.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JunitLearningTest {
+    static Set<JunitLearningTest> junitLearningTestInstanceSet = new HashSet<>();
+
+    @Test
+    public void test1() {
+        Assertions.assertFalse(junitLearningTestInstanceSet.contains(this));
+        junitLearningTestInstanceSet.add(this);
+    }
+
+    @Test
+    public void test2() {
+        Assertions.assertFalse(junitLearningTestInstanceSet.contains(this));
+        junitLearningTestInstanceSet.add(this);
+    }
+
+    @Test
+    public void test3() {
+        Assertions.assertFalse(junitLearningTestInstanceSet.contains(this));
+        junitLearningTestInstanceSet.add(this);
+    }
+}
