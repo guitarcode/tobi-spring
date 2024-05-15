eacc09f76cf04d8b74a07644984122c730f655b4
seminchoi <choicco89@gmail.com>
2024-05-16 00:18:14 +0900

    5장: upgradeLevels 메소드 구현 및 테스트

--- src/test/java/tobi/service/UserServiceTest.java
+++ src/test/java/tobi/service/UserServiceTest.java
@@ -1,20 +1,69 @@
 package tobi.service;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tobi.dao.UserDao;
+import tobi.domain.Level;
+import tobi.domain.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"/applicationContextTest.xml"})
 public class UserServiceTest {
     @Autowired
     UserService userService;
+    @Autowired
+    UserDao userDao;
+
+    List<User> users;
+
+    @BeforeEach
+    void setUp() {
+        users = Arrays.asList(
+                new User("semin", "최세민", "p1", Level.BASIC, 49, 0),
+                new User("tobi", "토비", "p2", Level.BASIC, 50, 0),
+                new User("younghan", "영한", "p3", Level.SILVER, 60, 29),
+                new User("spring", "스프링", "p4", Level.SILVER, 60, 30),
+                new User("java", "자바", "p5", Level.GOLD, 100, 100)
+        );
+    }
+
+    @AfterEach
+    public void destroy() {
+        userDao.deleteAll();
+    }
 
     @Test
     void bean() {
-        Assertions.assertThat(userService).isNotNull();
+        assertThat(userService).isNotNull();
+    }
+
+    @Test
+    void upgradeLevels() {
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+    }
+
+    void checkLevel(User user, Level expectedLevel) {
+        User userUpdate = userDao.get(user.getId());
+        assertThat(userUpdate.getLevel()).isEqualTo(expectedLevel);
     }
 }
